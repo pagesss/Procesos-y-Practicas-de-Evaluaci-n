936cc00fc5d595d1d142af297560af197484814b
David <daviddelarosa1104@gmail.com>
2024-12-02 04:58:19 -0500

    faltan las cosas en pdf

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 150);
 });
+
+
+let menu = document.querySelector("#menu-icon");
+let navlink = document.querySelector(".navlinks"); 
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navlink.classList.toggle("open")
+}
